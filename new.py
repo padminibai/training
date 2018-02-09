f02503c742fb1413366b1a7f4613fa0d2d7798c0
padminibai <35217570+padminibai@users.noreply.github.com>
2018-02-09 08:02:51 +0000

    Delete new.py

--- new.py
+++
@@ -1,9 +0,0 @@
-for word in 'apricots':
-	print('the word is :',word)
-B=input("enter the char:")
-if(B=='b'):
-	print("Blueberries")
-elif(B=='c'):
-	print("Cherries")
-else:
-	print("pls enter b or c:")
